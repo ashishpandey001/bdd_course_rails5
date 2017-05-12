9b06f4523ef87c47ceeb3976d704a53a16d975fb
Ashish Pandey <hmaster0@gmail.com>
2017-05-13 05:15:39 +0530

    Implement fallback message when no articles exist

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,9 @@
 module ArticlesHelper
+  def list_articles
+    if @articles.empty?
+      content_tag(:h1, 'No Articles Created', id: 'no-articles')
+    else
+      render @articles
+    end
+  end
 end
